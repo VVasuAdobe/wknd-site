0bfac4709773473568b0c713cf18af17be742280
Vivek Vasu <vvasu@adobe.com>
2023-04-19 14:45:12 +0530

    article

---
+++ blocks/article/article.js
@@ -0,0 +1,42 @@
+export default function decorate(block) {
+    const link = block.querySelector('a').href;
+    let indexUrl = JSON.parse(httpGet(link));
+    let articles = ""
+    if(indexUrl.data){
+        const wrapper = document.createElement('div');
+        wrapper.className = 'article-container';
+        
+        indexUrl.data.forEach(generateCard);
+
+        wrapper.innerHTML = articles;
+        block.append(wrapper);
+    }
+    function generateCard(item,index){
+        const card = `<div class="card card-${index}">
+                        <a href=${item.path}>
+                            <picture>
+                                <source type="image/webp" srcset="${item.image}?width=2000&amp;format=webply&amp;optimize=medium" media="(min-width: 600px)">
+                                <source type="image/webp" srcset="${item.image}?width=750&amp;format=webply&amp;optimize=medium">
+                                <source type="image/png" srcset="${item.image}?width=2000&amp;format=png&amp;optimize=medium" media="(min-width: 600px)">
+                                <img loading="lazy" alt="" type="image/png" src="${item.image}?width=750&amp;format=png&amp;optimize=medium" width="1280" height="853">
+                            </picture>
+                            <h6>${item.title}</h6>
+                           <!-- <p>${item.author}</p> -->
+                            <p class="description">${item.description}</p>
+                        </a>
+                      </div>`;
+        
+        articles += card;
+    }
+
+
+}
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); 
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
